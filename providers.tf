6173b47f3c76442a449d6096361dd396b011e74a
RootButcher <97753786+RootButcher@users.noreply.github.com>
2026-06-11 12:10:28 -0500

    added some comments and todos

--- providers.tf
+++ providers.tf
@@ -1,12 +1,13 @@
 terraform {
   required_providers {
+    #TODO move to BGP better cloud-init support but sshs into proxmox
     proxmox = {
       source  = "telmate/proxmox"
-      version = "3.0.2-rc06"
+      version = "3.0.2-rc06" #telemate seems to never move
     }
     talos = {
       source  = "siderolabs/talos"
-      version = "0.9.0"
+      version = "0.9.0" #very outdated
     }
   }
 }
